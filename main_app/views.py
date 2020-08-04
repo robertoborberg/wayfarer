0005f1f8b12e2278f45ec52a20257b0d77f767d0
Nathan Daniels <nathandanielsbassoon@gmail.com>
2020-08-04 18:57:38 -0500

    End of day

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render, HttpResponse
-
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def city_index(request):
+    return render(request, 'city/index.html')
+
+def signup(request):
+    error_message = ''
+    form = UserCreationForm()
+    context = {
+        'form': form,
+        'error_message': error_message,
+    }
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            global error 
+            error = 'User account already exists'
+            return render(request, 'registration/signup.html', context)
+    else:
+        return render(request, 'registration/signup.html', context)
