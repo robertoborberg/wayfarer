6263fc147dac9ee8aa5e1eb02423e614008c40be
roberto borberg <robertoborberg@gmail.com>
2020-08-06 18:59:01 -0400

    models

--- main_app/models.py
+++ main_app/models.py
@@ -10,3 +10,17 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.name
+
+
+
+class City(models.Model):
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    image = models.CharField(max_length=500)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=1000)
+    
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
