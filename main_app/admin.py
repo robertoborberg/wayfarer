600a09fb4935db891b9424a4c77adbbf82c6bd81
roberto borberg <robertoborberg@gmail.com>
2020-08-07 12:40:29 -0400

    styles

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile, City, Post
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(City)
+admin.site.register(Post)
