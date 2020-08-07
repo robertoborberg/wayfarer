e8ff2da8148ffc6f95dd6914c14add8f89297b1f
Nathan Daniels <nathandanielsbassoon@gmail.com>
2020-08-07 11:41:46 -0500

    Crud is working

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path('city/post/', views.city_post, name="city_post"),
     path('profile/<int:user_id>', views.profile, name="profile"),
     path('profile/<int:user_id>/update/', views.update, name="update_profile"),
+    path('city/<int:city_id>/new/', views.post_new, name='post_new'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('post/<int:post_id>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:post_id>/delete/', views.post_delete, name='post_delete'),
+    path('city/', views.city_index, name='city_index'),
+    path('city/<int:city_id>/', views.city_detail, name='city_detail')
 ]
